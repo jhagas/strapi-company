6e91489af5be3da6aa6ed1a4b6282da47315e45d
Jhagas Winaya <jhagashana@mailbox.org>
2022-04-23 04:05:21 +0700

    init

---
+++ src/api/mainheader/controllers/mainheader.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  mainheader controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mainheader.mainheader');
---
+++ src/api/mainheader/routes/mainheader.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mainheader router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mainheader.mainheader');
